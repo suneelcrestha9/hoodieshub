01da678301b1f3da9deb41ccb03e1c8b79f31f0b
suneelcrestha9 <126167086+suneelcrestha9@users.noreply.github.com>
2023-12-24 18:04:18 +0545

    footerimg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Main from '../components/Main'
 import Insale from '../components/Insale'
-import FooterTop from '../components/FooterTop'
+import Footerimg from '../components/Footerimg'
 
 export default function Home() {
   return (
     <div>
       <Main/>
       <Insale/>
-      <FooterTop/>
+      <Footerimg/>
     </div>
   )
 }
